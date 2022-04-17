d3fb2fbdb37fe0c6aa350cddfe1e5d7d457407b4
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-04-17 21:07:52 +0530

    StringBuilder_In_java

--- StringBuilderInJava.java
+++ StringBuilderInJava.java
@@ -2,15 +2,19 @@ public class StringBuilderInJava {
     public static void main(String[] args) {
         // it takes so much time to run so we can use stringbuilder in java
         // it makes so much strings in java due to this previious string is deleted then new String is created
-        String str = "Da";
-        String str2 = str + "vi";
+        String str1 = "Da";
+        String str2 = str1 + "vi";
         String str3 = str2+"nder";
         System.out.println(str3);
         
         // StringBuilder
         // it points to only one String
 
-        StringBuilder sb = new StringBuilder("Davinder");
-        System.out.println(sb);
+        StringBuilder str = new StringBuilder();
+        // add the String to StringBuilder Object
+        str.append("Davi");
+        char ch = str.charAt(1);
+        System.out.println("StringBuilder Object" + " contains = " + str);
+        System.out.println("Character at Position 1" + " in StringBuilder = " + ch);
     }
 }
